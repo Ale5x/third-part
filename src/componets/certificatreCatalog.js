44fd26831a3197bf61f661808dc36e07d6d1a83d
Ale5x <58393300+Ale5x@users.noreply.github.com>
2022-08-01 17:04:14 +0300

    08.01 from J

--- src/componets/certificatreCatalog.js
+++ src/componets/certificatreCatalog.js
@@ -5,7 +5,11 @@ import product_card from "../data/data-content";
 function CertificatreCatalog() {
     const [items, setItems] = useState(product_card._embedded.giftCertificateDtoList);
     const [dispatch, setDispatch] = useState();
-    const pages = [1, 2, 3, 4, 5]
+    const [id, setId] = useState(0);
+    const pages = [1, 2, 3, 4, 5];
+    const [message, setMessage] = useState("Test message");
+
+    const [countItems, setCountItems] = useState((sessionStorage.getItem('countItems') === null) ? 10 : sessionStorage.getItem('countItems'))
 
     const [fetching, setFetching] = useState(true);
     const [nameValue, setSeacrhValue] = useState("");
@@ -13,11 +17,18 @@ function CertificatreCatalog() {
     const [currentPageForName, setCurrentPageForName] = useState(1);
 
 
-    
 
-    const countItemsonPage = () => {
-        const select = document.getElementById("type_out");
-        console.log("SELRCT", select.options[select.selectedIndex].value)
+    const typeOutputItems = () => {
+        const select = document.getElementById("type-out");
+        console.log("SELECT type-out", select.options[select.selectedIndex].value)
+    }
+
+    const countItemsSelect = () => {
+        const select = document.getElementById("type-count-out");
+        // sessionStorage.setItem('countItems', select.options[select.selectedIndex].value);
+        console.log("SELECT type-count-out", select.options[select.selectedIndex].value)
+        setItems([]);
+        //start output new items
     }
 
 
@@ -27,6 +38,24 @@ function CertificatreCatalog() {
         setCurrentPageForName(1);
         setCurrentPage(1);
         setFetching(true)
+    }
+
+    const acctionDelete = (e) =>  {
+        console.log("acctionDelete id value", e.target.value);
+        setMessage(`Item with id ${e.target.value} deleted`)
+        sessionStorage.setItem('id', e.target.value);
+        // console.log("COOKIE", sessionStorage.getItem('id'))
+
+        console.log("TEST count ITEMS FROM COOKIES", sessionStorage.getItem('countItems'));
+        console.log("TEST count ITEMS", countItems);
+    }
+
+    const navigationNextPage = () => {
+        console.log("Next page")
+    }
+
+    const navigationPrevPage = () => {
+        console.log("Prev page")
     }
 
 
@@ -56,51 +85,87 @@ function CertificatreCatalog() {
 
 
   return (
-    <div>
+    <div className='main-content'>
         <div>
-            Message position 
+            <div className='message'>
+                {(message != "") ? message : ""}
+            </div>
         </div>
-        <div>
-            <input type='search' placeholder='Enter name' onChange={searchByName}></input>
+        <div className='search-content'>
+            <input type='search' placeholder='Enter name' onChange={searchByName} className='input-search'></input>
         </div>
-        <div>
-        <select id='type_out' onChange={countItemsonPage}>
-            <option value="date">Пункт 1</option>
-            <option value="date_reverse">Пункт 2</option>
-        </select>
+        <div className='settings-content'>
+            <div>
+                <span className='settings-span'>
+                    Type output 
+                    <select id='type-out' onChange={typeOutputItems}>
+                        <option selected="selected" value="date">Пункт 1</option>
+                        <option value="date">Пункт 11</option>
+                        <option value="date_reverse">Пункт 2</option>
+                    </select>
+                </span>
+                <span className='settings-span'>
+                    Count items output 
+                    <select id='type-count-out' onChange={countItemsSelect}>
+                    <option selected="selected" value="count-10">10</option>
+                    <option value="count-50">50</option>
+                    <option value="count-100">100</option>
+                    </select>
+                </span>
+            </div>
         </div>
         <div>
             <table>
                 <thead>
                     <tr>
-                        <td>Name</td>
-                        <td>Date created</td>
-                        <td>Date Updated</td>
-                        <td>Price</td>
-                        <td>Duration</td>
-                        <td>Action</td>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Date created</th>
+                        <th>Date Updated</th>
+                        <th>Price</th>
+                        <th>Duration</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {items.map(item => 
-                        <tr>
+                        <tr key={item.giftCertificateDtoId}>
+                            <td>{item.giftCertificateDtoId}</td>
                             <td>{item.name}</td>
                             <td>{item.createDate}</td>
                             <td>{item.lastUpdateDate}</td>
                             <td>{item.price}</td>
                             <td>{item.duration}</td>
                             <td>
-                                <button type='submit'>Delete</button>
-                                <button type='submit'>Edit</button>
-                                <button type='submit'>View</button>
+                                <button onClick={acctionDelete} value={item.giftCertificateDtoId} className='btn-delete'>Delete</button>
+                                <button onClick={acctionDelete} value={item.giftCertificateDtoId} className='btn-edit'>Edit</button>
+                                <button onClick={acctionDelete} value={item.giftCertificateDtoId} className='btn-view'>View</button>
                             </td>
                         </tr>)}
                 </tbody>
             </table>
-            <div className='page'>
-                {pages.map((page, index) => 
-                <span key={index} className={currentPage == page ? 'current-page' : 'page'}>{page}</span>               
-                )}
+            <div className='pages'>
+                <span>
+                    <span className='position-prev-btn'>
+                        <button className='btn-navigation' onClick={navigationPrevPage}>Prev</button>
+                    </span>
+                    <span className=''>
+                        <span className='current-page'>
+                            {currentPage}
+                        </span>
+                    </span>
+                    <span className='position-next-btn'>
+                        <button className='btn-navigation' onClick={navigationNextPage}>Next</button>
+                    </span>
+                </span>
+            </div>
+            <div>
+            <ui className='test'>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About Us</li>
+            <li>Contact</li>
+        </ui>
             </div>
         </div>
     </div>
